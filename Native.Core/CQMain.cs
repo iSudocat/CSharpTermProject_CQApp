ad2bbfdbee0620737eafcb5e03d0260217f8f95c
isudocat <i@sudocat.me>
2020-05-07 23:20:48 +0800

    no message

---
+++ Native.Core/CQMain.cs
@@ -0,0 +1,21 @@
+﻿using cc.wnapp.test.Code;
+using Native.Sdk.Cqp.Interface;
+using Unity;
+
+namespace Native.Core
+{
+	/// <summary>
+	/// 酷Q应用主入口类
+	/// </summary>
+	public class CQMain
+	{
+		/// <summary>
+		/// 在应用被加载时将调用此方法进行事件注册, 请在此方法里向 <see cref="IUnityContainer"/> 容器中注册需要使用的事件
+		/// </summary>
+		/// <param name="container">用于注册的 IOC 容器 </param>
+		public static void Register (IUnityContainer unityContainer)
+		{
+			unityContainer.RegisterType<IGroupMessage, Event_GroupMessage>("群消息处理");
+		}
+	}
+}

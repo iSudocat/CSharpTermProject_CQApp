1149d68adbe5ccd0f290455d32e4393e82bc1dbd
isudocat <i@sudocat.me>
2020-05-17 22:48:23 +0800

    添加了Winform窗口示例，外部调用 CQApi CQLog的示例（由Common类支持，其通过CQ启动事件储存）

---
+++ cc.wnapp.test.UI/Program.cs
@@ -0,0 +1,56 @@
+﻿using Native.Sdk.Cqp;
+using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cc.wnapp.test.UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+    }
+    
+
+    public class OpenWindowA : IMenuCall
+    {
+        
+        private Form1 _mainWindow = null;
+
+        public void MenuCall(object sender, CQMenuCallEventArgs e)
+        {
+            e.CQLog.Debug("菜单点击事件", $"打开界面-{e.Name}");
+
+            if (this._mainWindow == null)
+            {
+                this._mainWindow = new Form1();
+                this._mainWindow.Closing += MainWindow_Closing;
+                this._mainWindow.Show();	// 显示窗体
+            }
+            else
+            {
+                this._mainWindow.Activate();	// 将窗体调制到前台激活
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 对变量置 null, 因为被关闭的窗口无法重复显示
+            this._mainWindow = null;
+        }
+    }
+}

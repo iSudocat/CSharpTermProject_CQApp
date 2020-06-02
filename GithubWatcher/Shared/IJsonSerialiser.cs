79236934a277a059f07cb27121fb26fdbf41bb41
isudocat <i@sudocat.me>
2020-06-03 00:01:48 +0800

    Revert "更新了部分Github Webhook"
    This reverts commit 380878f587fc34c1ac7324d2e625d143b99f6f7e.

--- GithubWatcher/Shared/IJsonSerialiser.cs
+++
@@ -1,8 +0,0 @@
-using System.IO;
-
-namespace GitHubAutoresponder.Shared {
-    public interface IJsonSerialiser {
-        string Serialise(object obj);
-        T Deserialise<T>(string json);
-    }
-}
--- GithubWatcher/Webhook/Repository.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace GitHubAutoresponder.Webhook {
-    public class Repository {
-        [Required]
-        public string Name { get; set; }
-    }
-}

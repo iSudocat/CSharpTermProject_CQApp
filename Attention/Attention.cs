f507ace0d52ee07045d37f331579fcda31b6e270
CJQ-CS-WHU <jiaqi.cs.whu@gmail.com>
2020-06-03 22:00:09 +0800

    2020/6/3 陈家棋 模块框架搭建完成

---
+++ Attention/Attention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Attention
+{
+    //实体类
+    public class Attention
+    {
+        //消息关注者
+        [Key, Column(Order = 0)]
+        String noticer { get; set; }
+
+        //消息群
+        [Key, Column(Order = 1)]
+        String group { get; set; }
+
+        //关注点
+        [Key, Column(Order = 2)]
+        String attention { get; set; }
+    }
+}
--- cc.wnapp.whuHelper.Code/msgProcess.cs
+++ cc.wnapp.whuHelper.Code/msgProcess.cs
@@ -67,5 +67,32 @@ namespace cc.wnapp.whuHelper.Code
                 }
             }
         }
+
+        //检测是否要注册新的关注点
+        public static void PrivateAttentionHandler(string msgSourse, string message, string BotQQ)
+        {
+            //如果包含“取消关注”、“删除关注”...之类的词语，
+            //      解析出群号和消息内容/只有群号/只有消息内容
+            //      启动AttentionService的Remove线程
+
+            //如果包含“关注”、“监听”、“订阅”...的词语，
+            //      解析出关注的语句和关注的群
+            //      启动AttentionService的Add线程
+
+            //如果包含 “更改关注”、“更新关注”....的词语，
+            //      解析出两个变更的群号/变更的消息内容
+            //      启动AttentionService的Update线程
+
+            //如果包含“查看所有监听”/“查看所有关注”的词语，
+            //      如果其中有群号，则将群号解析出来并传入线程
+            //      启动AttentionService的Get线程
+
+        }
+
+        //检测消息中是否有关注点
+        public static void GroupAttentionHandler(string msgSourse, string message, string BotQQ)
+        {
+            //创建Attention线程Listen,将三个参数传入
+        }
     }
 }

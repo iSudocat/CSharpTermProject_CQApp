ce2fb67884895f83471f1d59dce0619b4c7fff5a
isudocat <i@sudocat.me>
2020-06-05 12:19:02 +0800

    修改所有DbConfigurationType为共同的SQLiteConfiguration（位于Tools/DbCommon.cs）

--- Attention/AttentionContext.cs
+++ Attention/AttentionContext.cs
@@ -13,19 +13,8 @@ using System.Data.Entity.Core.Mapping;
 using System.Data.Entity.Core.Metadata.Edm;
 namespace AttentionSpace
 {
-    public class SQLiteConfiguration : DbConfiguration
-    {
-        public SQLiteConfiguration()
-        {
-            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
-            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
-            Type t = Type.GetType("System.Data.SQLite.EF6.SQLiteProviderServices, System.Data.SQLite.EF6");
-            FieldInfo fi = t.GetField("Instance", BindingFlags.NonPublic | BindingFlags.Static);
-            SetProviderServices("System.Data.SQLite", (DbProviderServices)fi.GetValue(null));
-        }
-    }
 
-    [DbConfigurationType(typeof(SQLiteConfiguration))]
+    [DbConfigurationType(typeof(DbCommon.SQLiteConfiguration))]
     public class AttentionContext : DbContext
     {
         public static string CurrentDirectory = "";

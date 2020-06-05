30ff6d423cbfb4cf6af75b126b43f231437c052a
isudocat <i@sudocat.me>
2020-06-05 12:19:02 +0800

    修改所有DbConfigurationType为共同的SQLiteConfiguration（位于Tools/DbCommon.cs）

--- Calendar/Database.cs
+++ Calendar/Database.cs
@@ -17,19 +17,8 @@ namespace Schedule
 {
     public class Database
     {
-        public class SQLiteConfiguration : DbConfiguration
-        {
-            public SQLiteConfiguration()
-            {
-                SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
-                SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
-                Type t = Type.GetType("System.Data.SQLite.EF6.SQLiteProviderServices, System.Data.SQLite.EF6");
-                FieldInfo fi = t.GetField("Instance", BindingFlags.NonPublic | BindingFlags.Static);
-                SetProviderServices("System.Data.SQLite", (DbProviderServices)fi.GetValue(null));
-            }
-        }
 
-        [DbConfigurationType(typeof(SQLiteConfiguration))]
+        [DbConfigurationType(typeof(DbCommon.SQLiteConfiguration))]
         public class ScheduleContext : DbContext
         {
             public static string CurrentDirectory = "";

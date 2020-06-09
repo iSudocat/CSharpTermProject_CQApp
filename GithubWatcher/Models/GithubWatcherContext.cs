3b3d9a1511a222d1cf8a6b394cfe9b0afe5d843d
Y_Zhong <zhongyuan12345@qq.com>
2020-06-09 19:08:46 +0800

    数据库实体类设计

---
+++ GithubWatcher/Models/GithubWatcherContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Data.Entity;
+using SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+using System.Reflection;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.Core.Metadata.Edm;
+
+namespace GithubWatcher.Models
+{
+    [DbConfigurationType(typeof(DbCommon.SQLiteConfiguration))]
+    public class GithubWatcherContext : DbContext
+    {
+        public static string CurrentDirectory = "";
+        public jwContext() : base(new SQLiteConnection(@"Data Source=" + CurrentDirectory + @"\jwxt.db;"), false)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Score> Scores { get; set; }
+    }
+}
---
+++ GithubWatcher/Models/RepositorySubscription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+
+namespace GithubWatcher.Models
+{
+    public class RepositorySubscription : IComparable<RepositorySubscription>
+    {
+        [Required]
+        [Key, Column(Order = 2)]
+        public string RepositoryName{ get; set; }
+        [Required]
+        [Key, Column(Order = 1)]
+        public string QQ { get; set; }
+        public string Secret { get; set; }    //多对一关联
+
+        public override string ToString()
+        {
+            return $"{QQ} {RepositoryName} {Secret}\n";
+        }
+
+        public int CompareTo(RepositorySubscription other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

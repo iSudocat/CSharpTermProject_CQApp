1afb50a52619781e43eaf317cdbef58a10bc8ba4
Y_Zhong <zhongyuan12345@qq.com>
2020-06-12 16:48:38 +0800

    GithubWatcher模块基本完成，Webhook自动添加功能尚存bug，但不影响功能展示。

---
+++ GithubWatcher/OAuthService/WebhookData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GithubWatcher.OAuthService
+{
+    public class WebhookData
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public List<string> Events { get; set; }
+        public WebhookConfig Config { get; set; }
+
+        public WebhookData()
+        {
+            Name = "WHU Helper Robot";
+            Active = true;
+            Events = new List<string> { "push", "pull_request", "issues", "issue_comment", "create" };
+            Config = new WebhookConfig();
+        }
+    }
+}

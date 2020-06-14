79d5c39f46eed5293e2613dfbba446a3df265145
Eric_Lian <git@superexercisebook.com>
2020-06-15 00:29:41 +0800

    指令路由

---
+++ cc.wnapp.whuHelper.Code/CommandRouter/AbstractCommand.cs
@@ -0,0 +1,30 @@
+﻿using Native.Sdk.Cqp.EventArgs;
+
+namespace cc.wnapp.whuHelper.Code.CommandRouter
+{
+    public abstract class AbstractCommand
+    {
+        ///<summary>事件类型</summary>
+        public EventType EventType { get; set; }
+        
+        ///<summary>真实事件类型</summary>
+        public EventType ActualEventType { get; set; }
+
+        ///<summary>匹配模式</summary>
+        public MatchType MatchType { get; set; }
+
+        ///<summary>匹配字符串</summary>
+        public string MatchStr { get; set; }
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        public CQEventEventArgs CQEventArgsArgs { get; set; }
+
+
+        public int Handle()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ cc.wnapp.whuHelper.Code/CommandRouter/CommandServiceProvider.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Native.Sdk.Cqp.EventArgs;
+
+namespace cc.wnapp.whuHelper.Code.CommandRouter
+{
+    public class CommandServiceProvider
+    {
+        public EventType EventType {get; private set; }
+        public MatchType MatchType {get; private set; }
+        public string MatchStr {get; private set; }
+        public Type ServiceProvider { get; private set; }
+
+        public CommandServiceProvider(EventType EventType, MatchType MatchType, string MatchStr, Type ServiceProvider)
+        {
+            this.EventType = EventType;
+            this.MatchType = MatchType;
+            this.MatchStr = MatchStr;
+            this.ServiceProvider = ServiceProvider;
+        }
+
+        public void Handle(object sender, CQGroupMessageEventArgs e)
+        {
+            bool Flag = false;
+            if ((EventType & EventType.GroupMessage) != EventType.GroupMessage) return;
+            if ((MatchType & MatchType.Contains) == MatchType.Contains)
+            {
+                if (((string) e.Message).Contains(MatchStr)) Flag = true ;
+            }
+
+            if ((MatchType & MatchType.StartsWith) == MatchType.StartsWith)
+            {
+                if (((string)e.Message).StartsWith(MatchStr)) Flag = true;
+            }
+
+            if (Flag)
+            {
+                AbstractCommand Command = (AbstractCommand)ServiceProvider.Assembly.CreateInstance(ServiceProvider.Assembly.FullName);
+                Command.EventType = EventType;
+                Command.ActualEventType = EventType.GroupMessage;
+                Command.MatchType = MatchType;
+                Command.CQEventArgsArgs = e;
+                Command.Handle();
+            }
+        }
+
+        public void Handle(object sender, CQPrivateMessageEventArgs e)
+        {
+            bool Flag = false;
+            if ((EventType & EventType.PrivateMessage) != EventType.PrivateMessage) return;
+            if ((MatchType & MatchType.Contains) == MatchType.Contains)
+            {
+                if (((string)e.Message).Contains(MatchStr)) Flag = true;
+            }
+
+            if ((MatchType & MatchType.StartsWith) == MatchType.StartsWith)
+            {
+                if (((string)e.Message).StartsWith(MatchStr)) Flag = true;
+            }
+
+            if (Flag)
+            {
+                AbstractCommand Command = (AbstractCommand)ServiceProvider.Assembly.CreateInstance(ServiceProvider.Assembly.FullName);
+                Command.EventType = EventType;
+                Command.ActualEventType = EventType.PrivateMessage;
+                Command.MatchType = MatchType;
+                Command.CQEventArgsArgs = e;
+                Command.Handle();
+            }
+        }
+    }
+}

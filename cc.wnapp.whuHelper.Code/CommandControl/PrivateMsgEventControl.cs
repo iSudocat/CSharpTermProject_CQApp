20dcdd7b7e635b53723627a35ca74192d60b2592
Eric_Lian <git@superexercisebook.com>
2020-06-15 10:55:05 +0800

    拍平一些属性

--- cc.wnapp.whuHelper.Code/CommandControl/PrivateMsgEventControl.cs
+++ cc.wnapp.whuHelper.Code/CommandControl/PrivateMsgEventControl.cs
@@ -1,4 +1,5 @@
-﻿using cc.wnapp.whuHelper.Code.CommandRouter;
+﻿using System;
+using cc.wnapp.whuHelper.Code.CommandRouter;
 using Native.Sdk.Cqp.EventArgs;
 using Native.Sdk.Cqp.Model;
 
@@ -8,11 +9,11 @@ namespace cc.wnapp.whuHelper.Code.CommandControl
     {
         public string fromQQ { get; set; }
         public string message { get; set; }
-        public QQ botQQ { get; set; }
+        public string botQQ { get; set; }
 
         public override int Handle()
         {
-            botQQ = CQ.Api.GetLoginQQ();
+            botQQ = Convert.ToString(CQ.Api.GetLoginQQ().Id);
             message = ((CQPrivateMessageEventArgs)CQEventArgsArgs).Message;
             fromQQ = ((CQPrivateMessageEventArgs)CQEventArgsArgs).FromQQ;
             return HandleImpl();

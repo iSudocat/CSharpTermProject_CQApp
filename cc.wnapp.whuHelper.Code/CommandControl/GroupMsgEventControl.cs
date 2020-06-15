1fde96d2eda2134e18979cc594c01030e63fc0f0
Eric_Lian <git@superexercisebook.com>
2020-06-15 10:49:01 +0800

    添加指令路由使用样例

---
+++ cc.wnapp.whuHelper.Code/CommandControl/GroupMsgEventControl.cs
@@ -0,0 +1,25 @@
+﻿using cc.wnapp.whuHelper.Code.CommandRouter;
+using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.Model;
+
+namespace cc.wnapp.whuHelper.Code.CommandControl
+{
+    public abstract class GroupMsgEventControl : AbstractCommand
+    {
+        public string fromGroup { get; set; }
+        public string fromQQ { get; set; }
+        public string message { get; set; }
+        public QQ botQQ { get; set; }
+
+        public override int Handle()
+        {
+            botQQ = CQ.Api.GetLoginQQ();
+            message = ((CQGroupMessageEventArgs)CQEventArgsArgs).Message;
+            fromQQ = ((CQGroupMessageEventArgs)CQEventArgsArgs).FromQQ;
+            fromGroup = ((CQGroupMessageEventArgs)CQEventArgsArgs).FromGroup;
+            return HandleImpl();
+        }
+
+        public abstract int HandleImpl();
+    }
+}
---
+++ cc.wnapp.whuHelper.Code/CommandControl/SchedulerControl/AddScheduleToDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Schedule;
+using Tools;
+
+namespace cc.wnapp.whuHelper.Code.CommandControl.SchedulerControl
+{
+    /// <summary>
+    /// 添加群日程
+    /// 命令格式：添加群日程|2020/6/2 18:30:00(日常生活):吃饭 
+    /// </summary>
+    public class AddScheduleToDB : GroupMsgEventControl
+    {
+        public override int HandleImpl()
+        {
+            try
+            {
+                var dateTime = textOp.GetMiddleText(message, "|", "(");
+                var scheduleType = textOp.GetMiddleText(message, "(", ")");
+                string str = message.Split(')')[1];
+                var scheduleContent = textOp.GetRightText(str, ":");
+                GroupUserService groupUser = new GroupUserService(long.Parse(fromGroup), long.Parse(fromQQ));
+                if (groupUser.AddSchedule(PrivateMsgProcess.StrToDateTime(dateTime), scheduleType, scheduleContent))
+                {
+                    CQ.Api.SendGroupMessage(Convert.ToInt64(fromGroup), "【添加成功】");
+                }
+            }
+            catch (Exception e)
+            {
+                CQ.Api.SendGroupMessage(Convert.ToInt64(fromGroup), "【格式有误，添加失败】");
+                return 0;
+            }
+
+            return 0;
+        }
+    }
+}

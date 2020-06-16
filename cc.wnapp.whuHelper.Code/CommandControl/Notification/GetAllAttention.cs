d89f6e5a1ab8f54cefaab7776584027d2b4a8fc0
Eric_Lian <git@superexercisebook.com>
2020-06-17 00:03:40 +0800

    合并提醒模块

---
+++ cc.wnapp.whuHelper.Code/CommandControl/Notification/GetAllAttention.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AttentionSpace;
+
+namespace cc.wnapp.whuHelper.Code.CommandControl.Notification
+{
+    /// <summary>
+    /// 查询所有关注点
+    /// 命令格式：查询所有关注点
+    /// </summary>
+    public class GetAllAttention : PrivateMsgEventControl
+    {
+        public override int HandleImpl()
+        {
+            try
+            {
+                AttentionService attentionService = new AttentionService();
+                List<Attention> attList = attentionService.QueryAll();
+                String attListInMessage = "";
+                foreach (Attention att in attList)
+                {
+                    attListInMessage += "关注点：" + att.AttentionPoint + "\t群号：" + att.Group + "\n";
+                }
+                CQ.Api.SendPrivateMessage(Convert.ToInt64(fromQQ), attListInMessage);
+            }
+            catch (Exception e)
+            {
+                CQ.Api.SendPrivateMessage(Convert.ToInt64(fromQQ), "【查询失败】\n" + e);
+            }
+
+            return 0;
+        }
+
+    }
+}

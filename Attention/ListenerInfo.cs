817d6c1abd3f645822074535777be0606cf26674
CJQ-CS-WHU <jiaqi.cs.whu@gmail.com>
2020-06-18 00:50:08 +0800

    陈家棋 2020/6/18 关注点板块基本完成 有一个bug未处理

---
+++ Attention/ListenerInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttentionSpace
+{
+    public class ListenerInfo
+    {
+        public String Listener;
+
+        public int Count;
+
+        public ListenerInfo(String l, int c)
+        {
+            this.Listener = l;
+            this.Count = c;
+        }
+    }
+}

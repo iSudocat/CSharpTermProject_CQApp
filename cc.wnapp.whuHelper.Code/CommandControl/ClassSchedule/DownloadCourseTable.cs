f72817da3a73a7abdc9480602cddb71b45d33116
嘉程 张 <694510779@qq.com>
2020-06-19 14:21:36 +0800

    添加下载课程表功能，修了一些bug

---
+++ cc.wnapp.whuHelper.Code/CommandControl/ClassSchedule/DownloadCourseTable.cs
@@ -0,0 +1,27 @@
+﻿using Eas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CosOperation;
+
+namespace cc.wnapp.whuHelper.Code.CommandControl.ClassSchedule
+{
+    public class DownloadCourseTable : PrivateMsgEventControl
+    {
+        public override int HandleImpl()
+        {
+            Student student;
+            using (jwContext context = new jwContext())
+            {
+                student = context.Students.Where(s => s.QQ == fromQQ).FirstOrDefault();
+            }
+
+            string downloadUrl = "";
+            downloadUrl = CosOp.UploadFile($"{student.StuID}CourseTable.xlsx", CQ.Api.AppDirectory + $@"Export\{student.StuID}CourseTable.xlsx");
+            CQ.Api.SendPrivateMessage(Convert.ToInt64(fromQQ), "请前往下面网址下载Excel课程表：\n" + downloadUrl);
+            return 0;
+        }
+    }
+}
